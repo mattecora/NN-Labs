d7602fd6fd040e2a9de5277f51166a7be1d5a77d
Matteo Corain <m96.corain@gmail.com>
2019-09-13 23:46:00 -0500

    Hw #1: question 3 mostly answered

--- hw01/hw1_ex3.py
+++ hw01/hw1_ex3.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+plt.rc('text', usetex=True)
+
 def u(x):
     return 1 if x >= 0 else 0
 
@@ -28,22 +30,23 @@ def pta(eta, w0, S, d):
 
     return w, epochs, mclass
 
-def plot_data_and_sep(S0, S1, w, eta=None):
+def plot_data_and_sep(S0, S1, w):
     plt.figure()
     plt.plot([x[1] for x in S0], [x[2] for x in S0], color="r", marker="s", linestyle="none", fillstyle="none")
     plt.plot([x[1] for x in S1], [x[2] for x in S1], color="b", marker="o", linestyle="none", fillstyle="none")
     plt.plot([-1, 1], [-(w[0] + w[1] * x) / w[2] for x in [-1, 1]], color="g", linestyle="--")
     plt.axis([-1, 1, -1, 1])
-    plt.title("Separation (eta: {})\nWeights: {}".format(eta, w))
-    plt.legend(["S0", "S1", "Boundary"])
+    plt.title("Separation with weights: {}".format(w))
+    plt.legend(["$S_0$", "$S_1$", "Boundary"])
     plt.show(block=False)
 
 def plot_mclass(mclass, eta):
     plt.figure()
-    plt.plot(mclass, marker="o", linestyle="--", fillstyle="none")
-    plt.axis([0, len(mclass) - 1, 0, max(mclass)])
-    plt.title("Misclassifications per epoch (eta: {})".format(eta))
+    for e in eta:
+        plt.plot(mclass[e], marker="x", fillstyle="none")
+    plt.title("Misclassifications per epoch")
     plt.grid()
+    plt.legend(["$\eta = {}$".format(e) for e in eta])
     plt.show(block=False)
 
 # Get some random weights
@@ -51,14 +54,14 @@ wr = np.array([np.random.uniform(-1/4, 1/4), np.random.uniform(-1, 1), np.random
 print("Exact weights: {}".format(wr))
 
 # Build the training set
-n_samples = 1000
+n_samples = 100
 S = [np.array([1, np.random.uniform(-1, 1), np.random.uniform(-1, 1)]) for i in range(n_samples)]
 print("Number of samples: {}".format(n_samples))
 
 # Construct S0, S1 and d
-S0 = [x for x in S if wr @ x >= 0]
-S1 = [x for x in S if wr @ x < 0]
 d = [1 if wr @ x >= 0 else 0 for x in S]
+S0 = [S[i] for i in range(len(S)) if d[i] == 0]
+S1 = [S[i] for i in range(len(S)) if d[i] == 1]
 
 print("Samples in S0: {}".format(len(S0)))
 print("Samples in S1: {}".format(len(S1)))
@@ -68,15 +71,15 @@ plot_data_and_sep(S0, S1, wr)
 
 # Initialize random w0
 w0 = np.random.uniform(-1, 1, 3)
+mclass = {}
 
 # Run PTA with variable eta
 for eta in [1, 10, 0.1]:
     print("================")
-    w, epochs, mclass = pta(eta, w0, S, d)
+    w, epochs, mclass[eta] = pta(eta, w0, S, d)
 
-    # Plot separator and misclassification rate
-    plot_data_and_sep(S0, S1, w, eta)
-    plot_mclass(mclass, eta)
+# Plot misclassification rate
+plot_mclass(mclass, [1, 10, 0.1])
 
 # Maintain graphs
 plt.show()
