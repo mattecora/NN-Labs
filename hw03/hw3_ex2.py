74d534de188275e9c485d6f1f164c452ca28f2ae
Matteo Corain <m96.corain@gmail.com>
2019-10-13 11:02:10 -0500

    Hw #3: LLS through pseudoinverse computation

--- hw03/hw3_ex2.py
+++ hw03/hw3_ex2.py
@@ -22,41 +22,40 @@ def hessf(xi, yi, w):
     ]).reshape(2, 2)
 
 # Create vectors
-xi = np.array([i + 1 for i in range(50)])
-yi = np.array([x + np.random.uniform(-1, 1) for x in xi])
+xi = [i + 1 for i in range(50)]
+yi = [x + np.random.uniform(-1, 1) for x in xi]
 
 # Compute linear least squares fit
-xm = np.average(xi)
-ym = np.average(yi)
-m = sum([(xi[i] - xm) * (yi[i] - ym) for i in range(50)]) / sum([(xi[i] - xm)**2 for i in range(50)])
-q = ym - m*xm
-print("LS fit: m = {}, q = {}".format(m, q))
+X = np.array([1] * 50 + xi).reshape(2, 50)
+Y = np.array(yi).reshape(1, 50)
+w_ls = (Y @ np.linalg.pinv(X)).transpose()
+print("LS fit: m = {}, q = {}".format(w_ls[1][0], w_ls[0][0]))
 
 # Compute weights using gradient descent
 eta = 2.25e-5
 eps = 1e-6
-w = np.array([np.random.uniform(-1, 1), np.random.uniform(-1, 1)]).reshape(2, 1)
+w_gd = np.array([np.random.uniform(-1, 1), np.random.uniform(-1, 1)]).reshape(2, 1)
 
 epochs = 0
 while True:
     epochs = epochs + 1
-    wnew = w - eta * gradf(xi, yi, w)
-    if np.linalg.norm(wnew - w) < eps:
+    wnew = w_gd - eta * gradf(xi, yi, w_gd)
+    if np.linalg.norm(wnew - w_gd) < eps:
         break
 
-    w = wnew
+    w_gd = wnew
     if epochs % 1000 == 0:
-        print("Epoch {}: w = {}, g = {}".format(epochs, w.transpose(), gradf(xi, yi, w).transpose()))
+        print("Epoch {}: w = {}, g = {}".format(epochs, w_gd.transpose(), gradf(xi, yi, w_gd).transpose()))
 
-print("GD fit: m = {}, q = {} (epochs: {})".format(w[1][0], w[0][0], epochs))
+print("GD fit: m = {}, q = {} (epochs: {})".format(w_gd[1][0], w_gd[0][0], epochs))
 
 # Compute weights after one iteration of Newton's method
-w = np.array([np.random.uniform(-1, 1), np.random.uniform(-1, 1)]).reshape(2, 1)
-w = w - np.linalg.inv(hessf(xi, yi, w)) @ gradf(xi, yi, w)
-print("NM fit: m = {}, q = {}".format(w[1][0], w[0][0]))
+w_nm = np.array([np.random.uniform(-1, 1), np.random.uniform(-1, 1)]).reshape(2, 1)
+w_nm = w_nm - np.linalg.inv(hessf(xi, yi, w_nm)) @ gradf(xi, yi, w_nm)
+print("NM fit: m = {}, q = {}".format(w_nm[1][0], w_nm[0][0]))
 
 # Plot the LLS fit
 plt.plot(xi, yi, marker="x", linestyle="none")
-plt.plot(xi, [m*x + q for x in xi], linestyle="dashed")
+plt.plot(xi, [w_ls[1][0]*x + w_ls[0][0] for x in xi], linestyle="dashed")
 plt.title("Linear least squares fit of $(x_i, y_i)$")
 plt.show()
